c036647dcb94b0907debbb6b39f0efaf9e222cce
Parvathi <pava@uw.edu>
2020-07-21 22:05:54 -0700

    added server socket and port number

--- server/src/main/java/com/example/server/ServerApplication.java
+++ server/src/main/java/com/example/server/ServerApplication.java
@@ -3,11 +3,22 @@ package com.example.server;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
+import java.io.IOException;
+import java.net.ServerSocket;
+
 @SpringBootApplication
 public class ServerApplication {
 
     public static void main(String[] args) {
-        SpringApplication.run(ServerApplication.class, args);
+        int port=8000;
+        try {
+            ServerSocket serverSocket=new ServerSocket(port);
+        }
+        catch (IOException e) {
+            e.printStackTrace();
+        }
+
+
     }
 
 }
